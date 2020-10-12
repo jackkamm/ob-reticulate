d5e5d4ce3a7604aa6390425384e0e109a71bc9ed
Jack Kamm <jackkamm@gmail.com>
2020-10-11 21:03:53 -0700

    Switch to using ob-python blocks

--- ob-reticulate.el
+++ ob-reticulate.el
@@ -32,22 +32,33 @@
 
 (defalias 'org-babel-edit-prep:reticulate 'org-babel-edit-prep:R)
 
-(add-to-list 'org-src-lang-modes '("reticulate" . python))
+(advice-add
+ 'org-babel-execute:python :around 'org-babel-reticulate-advice)
 
-(defun org-babel-execute:reticulate (body params)
-  (let* ((tmp-src-file (org-babel-temp-file "reticulate-"))
-	 (result-type (cdr (assq :result-type params))))
-    (with-temp-file tmp-src-file (insert body))
-    (org-babel-execute:R
-     (format (concat "reticulate::py_run_string(\"%s\")"
-		     (when (equal result-type 'value) "
+(defun org-babel-reticulate-advice (orig-fun body params)
+  (let* ((session (cdr (assq :session params)))
+         (session-mode
+          (and session
+             (not (string= session "none"))
+             (let ((session-buffer (get-buffer session)))
+               (and session-buffer
+                    (with-current-buffer session-buffer
+                      major-mode))))))
+    (if (not (eq session-mode 'inferior-ess-r-mode))
+        (funcall orig-fun body params)
+      (let* ((tmp-src-file (org-babel-temp-file "reticulate-"))
+	     (result-type (cdr (assq :result-type params))))
+        (with-temp-file tmp-src-file (insert body))
+        (org-babel-execute:R
+         (format (concat "reticulate::py_run_string(\"%s\")"
+		         (when (equal result-type 'value) "
 reticulate::py$`__org_babel_python_final`"))
-             (org-babel-python-format-session-value
-              tmp-src-file
-              (org-babel-process-file-name
-               (org-babel-temp-file "reticulate-dummy-") 'noquote)
-              nil))
-     params)))
+                 (org-babel-python-format-session-value
+                  tmp-src-file
+                  (org-babel-process-file-name
+                   (org-babel-temp-file "reticulate-dummy-") 'noquote)
+                  nil))
+         params)))))
 
 (provide 'ob-reticulate)
 
