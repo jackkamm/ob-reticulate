6cc7e2efa077fb8276cb73e4ae778a442169cb32
Jack Kamm <jackkamm@gmail.com>
2020-12-22 18:06:49 -0800

    Add GPL

--- ob-reticulate.el
+++ ob-reticulate.el
@@ -1,12 +1,26 @@
 ;;; ob-reticulate.el --- Babel Functions for reticulate -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020 Free Software Foundation, Inc.
-
 ;; Author: Jack Kamm
 ;; Keywords: literate programming, reproducible research, R, statistics, languages, outlines, processes
 ;; Package-Requires: ((org "9.4") (emacs "24.4"))
 ;; Homepage: https://github.com/jackkamm/ob-reticulate
 ;; Version: 1.0.0
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
 
 ;;; Commentary:
 
